a2ccf9a5579aa8ea15348051e172e1ee32a26cd1
VertigoCodes <t.yuvaraja01@gmail.com>
2023-02-02 22:57:31 +0530

    Updated code

--- data-viz-source.py
+++ data-viz-source.py
@@ -1,1 +1,13 @@
+#LIBRARY IMPORTS
+import pandas as pd
+import numpy as np
+import seaborn as sb
+import matplotlib.pyplot as plt
+
 print("Welcome to FYP Project.")
+print("For the first phase of this project, we will be looking at the data that we will be working with in a presentable manner and try to gain some insights and inferences from the data to forumlate our hypotheses.")
+
+trichyHousingInfo = pd.read_csv('Trichy Housing Info .csv')
+
+
+
